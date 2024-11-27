9c9cf7f00bf28bd8b44b2c0249309bc926c8a21b
Yuri Catunda <131727946+yuuricathugaa@users.noreply.github.com>
2024-11-27 16:46:25 -0300

    Subindo Noticia

---
+++ routes/Noticia/noticia.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const noticiaController = require('../../controllers/noticia');
+
+// Define as rotas para as notícias
+router.get('/', noticiaController.getAllNoticias); // Listar todas as notícias
+router.post('/', noticiaController.createNoticia); // Criar uma nova notícia
+router.get('/:id', noticiaController.getNoticiaById); // Obter uma notícia por ID
+router.put('/:id', noticiaController.updateNoticia); // Atualizar uma notícia
+router.delete('/:id', noticiaController.deleteNoticia); // Deletar uma notícia
+
+module.exports = router;
