1533f779dc8c650210811b93f6427d22291b7faf
Yuri Catunda <131727946+yuuricathugaa@users.noreply.github.com>
2024-11-27 16:54:14 -0300

    Subindo Noticia

--- models/Noticia/noticia.js
+++ models/Noticia/noticia.js
@@ -16,10 +16,6 @@ const Noticia = sequelize.define('Noticia', {
     type: DataTypes.TEXT,
     allowNull: false,
   },
-  autor: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
   dataPublicacao: {
     type: DataTypes.DATE,
     allowNull: false,
@@ -31,5 +27,33 @@ const Noticia = sequelize.define('Noticia', {
     defaultValue: 0,
   },
 });
+
+// Relacionamentos
+Noticia.associate = (models) => {
+  // Relacionamento com Autor
+  Noticia.belongsTo(models.Usuario, {
+    foreignKey: 'autor_id',
+    as: 'autor',
+  });
+
+  // Relacionamento com Comentários
+  Noticia.hasMany(models.Comentario, {
+    foreignKey: 'noticia_id',
+    as: 'comentarios',
+  });
+
+  // Relacionamento com Categorias
+  Noticia.belongsTo(models.Categoria, {
+    foreignKey: 'categoria_id',
+    as: 'categoria',
+  });
+
+  // Relacionamento com Tags (M:N)
+  Noticia.belongsToMany(models.Tag, {
+    through: 'NoticiaTag',
+    foreignKey: 'noticia_id',
+    as: 'tags',
+  });
+};
 
 module.exports = Noticia;
