ae40d6d5191e8cafb57053af42b623c5d6552cb8
Yuri Catunda <131727946+yuuricathugaa@users.noreply.github.com>
2024-11-27 16:48:13 -0300

    Subindo Noticia

--- routes/Noticia/noticia.js
+++ routes/Noticia/noticia.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const noticiaController = require('../../controllers/noticia');
+const noticiaController = require('../../controllers/Noticia/noticia');
 
 // Define as rotas para as notícias
 router.get('/', noticiaController.getAllNoticias); // Listar todas as notícias
