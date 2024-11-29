631f3379e631b300493891662c730b5a17f51e7c
Mayaranascimento <mayarawyrla16@gmail.com>
2024-11-28 23:35:44 -0300

    Adicionando model, controller e rotas da classe Usuário

---
+++ controllers/Usuario/usuario.js
@@ -0,0 +1,118 @@
+const Usuario = require('../../models/Usuario/usuario');
+console.log(Usuario);
+
+// Buscar id
+exports.getUsuarioById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const usuario = await Usuario.findByPk(id);
+        if (!usuario) {
+            return res.status(404).json({ message: 'Usuário não encontrado' });
+        }
+        res.status(200).json(usuario);
+    } catch (error) {
+        console.error('Usuário não encontrado com este id:', error);
+        res.status(500).json({ message: 'Erro ao buscar o usuário', error: error.message });
+    }
+};
+
+// Buscar por nome
+exports.getUsuarioByNome = async (req, res) => {
+    try {
+        const { nomeUsuario } = req.params;
+        const usuario = await Usuario.findOne({ where: { nomeUsuario } });
+        if (!usuario) {
+            return res.status(404).json({ message: 'Usuário não encontrado' });
+        }
+        res.status(200).json(usuario);
+    } catch (error) {
+        console.error('Erro ao buscar o usuário por nome:', error);
+        res.status(500).json({ message: 'Erro ao buscar o usuário', error: error.message });
+    }
+};
+
+// Buscar por contato
+exports.getUsuarioByContato = async (req, res) => {
+    try {
+        const { contato } = req.params;
+        const usuario = await Usuario.findOne({ where: { contato } });
+        if (!usuario) {
+            return res.status(404).json({ message: 'Usuário não encontrado' });
+        }
+        res.status(200).json(usuario);
+    } catch (error) {
+        console.error('Erro ao buscar o usuário por contato:', error);
+        res.status(500).json({ message: 'Erro ao buscar o usuário', error: error.message });
+    }
+};
+
+// Buscar por email
+exports.getUsuarioByEmail = async (req, res) => {
+    try {
+        const { email } = req.params;
+        const usuario = await Usuario.findOne({ where: { email } });
+        if (!usuario) {
+            return res.status(404).json({ message: 'Usuário não encontrado' });
+        }
+        res.status(200).json(usuario);
+    } catch (error) {
+        console.error('Erro ao buscar o usuário por email:', error);
+        res.status(500).json({ message: 'Erro ao buscar o usuário', error: error.message });
+    }
+};
+
+// Listar todos os usuários
+exports.getAllUsuarios = async (req, res) => {
+    try {
+        const usuarios = await Usuario.findAll();
+        res.status(200).json(usuarios);
+    } catch (error) {
+        console.error('Erro ao listar todos os usuários:', error);
+        res.status(500).json({ message: 'Erro ao buscar todos os usuários', error: error.message });
+    }
+};
+
+// Criar usuário
+exports.createUsuario = async (req, res) => {
+    try {
+        const { nomeUsuario, email, senha, bio, contato, localizacao, fotoPerfil, tipo } = req.body;
+        const novoUsuario = await Usuario.create({ nomeUsuario, email, senha, bio, contato, localizacao, fotoPerfil });
+        res.status(201).json(novoUsuario);
+    } catch (error) {
+        console.error('Erro ao criar o usuário:', error);
+        res.status(500).json({ message: 'Erro ao criar o usuário', error: error.message });
+    }
+};
+
+// Atualizar usuário
+exports.updateUsuario = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { nomeUsuario, email, senha, bio, contato, localizacao, fotoPerfil } = req.body;
+        const usuario = await Usuario.findByPk(id);
+        if (!usuario) {
+            return res.status(404).json({ message: 'Usuário não encontrado' });
+        }
+        await usuario.update({ nomeUsuario, email, senha, bio, contato, localizacao, fotoPerfil });
+        res.status(200).json(usuario);
+    } catch (error) {
+        console.error('Erro ao atualizar informações do usuário:', error);
+        res.status(500).json({ message: 'Erro ao atualizar o usuário', error: error.message });
+    }
+};
+
+// Deletar usuário
+exports.deleteUsuario = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const usuario = await Usuario.findByPk(id);
+        if (!usuario) {
+            return res.status(404).json({ message: 'Usuário não encontrado' });
+        }
+        await usuario.destroy();
+        return res.status(200).json({ message: 'Usuário deletado com sucesso' });
+    } catch (error) {
+        console.error('Erro ao deletar o usuário:', error);
+        res.status(500).json({ message: 'Erro ao deletar usuário', error: error.message });
+    }
+};
---
+++ routes/Usuario/usuario.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const usuarioController = require('../controllers/Usuario/usuario'); 
+
+const router = express.Router();
+
+// Define as rotas e conecta aos métodos do controller Usuário
+router.get('/:id', usuarioController.getUsuarioById); // Rota para buscar pelo id
+router.get('/nome/:nomeUsuario', usuarioController.getUsuarioByNome); // Rota para buscar pelo nome
+router.get('/contato/:contato', usuarioController.getUsuarioByContato); // Rota para buscar por contato
+router.get('/email/:email', usuarioController.getUsuarioByEmail); // Rota para buscar por email
+router.get('/list/usuarios', usuarioController.getAllUsuarios); // Rota para listar todos os usuários
+router.post('/add/usuario', usuarioController.createUsuario); // Rota para criar um novo usuário
+router.put('/update/:id', usuarioController.updateUsuario); // Rota para atualizar informações do usuário pelo id
+router.delete('/delete/:id', usuarioController.deleteUsuario); // Rota para deletar um usuário pelo id
+
+module.exports = router; 
